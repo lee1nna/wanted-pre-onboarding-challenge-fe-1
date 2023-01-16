32cec7032b2c13eb9bed49ffafda9910b748515c
lee1nna <dlgkssk0209@gmail.com>
2023-01-16 22:24:25 +0900

    feat: customAxios 수정

--- src/modules/customAxios.ts
+++ src/modules/customAxios.ts
@@ -1,8 +1,9 @@
 import axios, {AxiosInstance} from 'axios'
 
+console.log('localStorage.getItem(\'token\')', localStorage.getItem('token'))
 export const customAxios: AxiosInstance = axios.create({
     baseURL: 'http://localhost:8080',
     headers: {
-        access_token: localStorage.getItem('token')
+        "Authorization": localStorage.getItem('token')
     }
 })
